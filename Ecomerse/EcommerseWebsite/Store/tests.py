218240e9ad107c2b47eae4bf3fe451d78d09a241
MuleBhanuprakash <54138678+MuleBhanuprakash@users.noreply.github.com>
2021-01-08 17:56:15 +0530

    new test commit

--- Ecomerse/EcommerseWebsite/Store/tests.py
+++ Ecomerse/EcommerseWebsite/Store/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import Customer, Product, Order
 
-# Create your tests here.
+
+class CustomerTest(TestCase):
+    def test_cust(self):
+        cus1 = Customer()
+        cus1.name = "bhanu"
+        cus1.email = "bhanu@email.com"
+        cus1.save()
+
+        self.assertEqual(cus1, Customer.objects.get())
+
+
+class ProductTest(TestCase):
+
+    def test_product(self):
+        prod1 = Product()
+        prod1.name = "jeans"
+        prod1.price = 35000.50
+        prod1.digital = False
+        prod1.save()
+
+        self.assertEqual(1, Product.objects.count())
+
+
+class OrderTest(TestCase):
+    def test_order(self):
+        ord = Order()
+        ord.customer = Customer()
+        ord.date_ordered="02/03/2007"
+        ord.complete=True
+
+        self.assertEqual(ord,Order.objects.get())
+
